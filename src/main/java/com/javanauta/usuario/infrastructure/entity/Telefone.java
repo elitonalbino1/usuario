68d5d1cd8b7cdb0b1f7200b13a01b48285566bea
Eliton Albino <eliton.albino@hotmail.com>
2026-03-19 23:38:36 -0300

    adicionadas classes de security

---
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,19 @@
+package com.javanauta.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Telefone")
+public class Telefone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numero", length = 10)
+    private String numero;
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+}
