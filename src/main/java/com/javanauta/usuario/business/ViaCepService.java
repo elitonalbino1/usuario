75be5b27d26488610b6eec66aed6cf207357b0f8
Eliton Albino <eliton.albino@hotmail.com>
2026-09-24 22:08:36 -0300

    atualizado api_cep parte1

---
+++ src/main/java/com/javanauta/usuario/business/ViaCepService.java
@@ -0,0 +1,33 @@
+package com.javanauta.usuario.business;
+
+import com.javanauta.usuario.infrastructure.clients.ViaCepClient;
+import com.javanauta.usuario.infrastructure.clients.ViaCepDTO;
+import com.javanauta.usuario.infrastructure.exceptions.IllegalArgumentException;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@RequiredArgsConstructor
+
+public class ViaCepService {
+
+    private final ViaCepClient client;
+
+    public ViaCepDTO buscarDadosEndereco(String cep) {
+        return client.buscaDadosEndereco(processarCep(cep));
+
+    }
+
+
+    private String processarCep(String cep) {
+        String cepFormatado = cep.replace(" ", "").
+                replace("-", "");
+
+        if (!cepFormatado.matches("[0-9+]") || !Objects.equals(cepFormatado.length(), 8)) {
+            throw  new IllegalArgumentException("O cep contem caracteres invalidos, favor verificar");
+        }
+        return cepFormatado;
+    }
+}
